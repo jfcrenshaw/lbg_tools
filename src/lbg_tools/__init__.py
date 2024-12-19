37d600da43b00e12ea12c6bcae99ed8a88eaffb0
John Franklin Crenshaw <jfcrenshaw@gmail.com>
2024-12-19 15:56:03 -0300

    Added IGM models.

--- src/lbg_tools/__init__.py
+++ src/lbg_tools/__init__.py
@@ -1,4 +1,5 @@
 from .completeness import Completeness
+from .igm import IGM
 from .library import library
 from .luminosity_function import LuminosityFunction
 from .tomography import TomographicBin
